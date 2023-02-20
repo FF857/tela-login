12d6b78365b5e0d628dd1cae9c84b650ee51a358
Felipe <eufelipe857@gmail.com>
2023-02-20 00:47:20 -0300

    add conteudo

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 
 // "banco de dados"
-var user = "usuario@teste.com"
-var password = "123456"
-
+var user_bd = "usuario@teste.com"
+var password_bd = "12345"
+//
 
 function check_login(){
 
     var email = document.getElementById("email").value;
     var senha = document.getElementById("password").value;
 
-    var answer = document.getElementById("resposta").innerHTML;
+    var answer = document.getElementById("resposta");
 
-    if (user == email && password == senha){
+    if (user_bd == email && password_bd == senha){
         document.location.href = "login.html";
     }
     else {
 
-        answer.innerHTML = "Acesso negado!"
+        answer.innerHTML = "Usuário ou senha incorretos!"
+        //alert("Usuário ou senha incorretos")
     }
 }
+
